cb35f047e04ea5b4e85d58a5089fba8597e26826
dung <dungvatoi12>
2017-03-28 01:31:15 +0700

    Add test countdown

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -36,6 +36,30 @@ describe('Countdown', () => {
                 expect(countdown.state.count).toBe(0);
                 done();
             }, 3001);
+        });
+
+        it('should pause countdown on paused status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done();
+            },2001)
+        });
+
+        it('should stop countdown on stopped status', () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(function() {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+            }, 3001);
         })
 
     })
